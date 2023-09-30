933bcf9696efc4036c185ed374fbc7e1d45f2696
mcr45 <depr3604@protonmail.com>
2023-09-30 18:45:51 -0500

    challenge:try to load recipe dinamycally

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,10 +7,13 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-recipes:Recipe[]=[new Recipe('A test','A brave test recipe','https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.lfxXrK4M6k8IAblvoCWGEwHaE8%26pid%3DApi&f=1&ipt=3c187084746d649ac9e35b678e5f43b0cbd91fa2fbccf9ff59fe5a735d3a5856&ipo=images')]
+recipes:Recipe[]=[new Recipe('A test','A brave test recipe','https://images.pexels.com/photos/18515070/pexels-photo-18515070/free-photo-of-coffeeshop.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')]
 
 constructor(){}
 
+getName(){return this.recipes[0].name}
+getDescription(){return this.recipes[0].description}
+getUrl(){return this.recipes[0].imagePath}
 
 ngOnInit(){}
 }
