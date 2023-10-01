61ba8bca5c00954882866bfc6fab6958672ebce0
mcr45 <depr3604@protonmail.com>
2023-09-30 20:20:15 -0500

    implemented ingredients model,modified shoplist template. all good

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { Ingredients } from '../shared/ingredients.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit{
+ingredients:Ingredients[]=[
+  new Ingredients('Banana',7),new Ingredients('pineapple',56)
+]
+
+
+
+
+
+ngOnInit(): void {
+
+}
 
 }
